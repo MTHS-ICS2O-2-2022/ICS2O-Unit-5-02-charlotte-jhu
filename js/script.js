98858da3c356e617e4a3cb9a02cd64eed96680cc
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-02 14:03:59 +0000

    added code for website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+
+function myButtonClicked() {
+  // This function creates a random number, either positive or negative
+  let randomNumber = 0.00
+  // input
+  const positiveButtonChecked = document.getElementById("positive-number").checked
+
+  // process
+  if (positiveButtonChecked) {
+    // positive
+    randomNumber = Math.floor(Math.random() * 6)
+  } else {
+    // negative
+    randomNumber = Math.floor(Math.random() * -6)
+  }
+
+  // output
+  document.getElementById('random-number').innerHTML = randomNumber
+}
